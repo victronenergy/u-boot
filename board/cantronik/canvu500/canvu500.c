5b08b39ed4e0f343f9b07c03e4c5f35f00b146bb
Mans Rullgard <mans@mansr.com>
2017-08-16 19:36:32 +0100

    Add CANvu500 Platform

---
+++ board/cantronik/canvu500/canvu500.c
@@ -0,0 +1,305 @@
+/*
+ * Copyright (C) 2016 CANtronik Ltd.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <asm/arch/clock.h>
+#include <asm/arch/crm_regs.h>
+#include <asm/arch/iomux.h>
+#include <asm/arch/imx-regs.h>
+#include <asm/arch/mx6-pins.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/mach-imx/iomux-v3.h>
+#include <asm/mach-imx/video.h>
+#include <asm/gpio.h>
+#include <asm/io.h>
+#include <linux/sizes.h>
+#include <common.h>
+#include <environment.h>
+#include <fsl_esdhc.h>
+#include <mmc.h>
+#include <miiphy.h>
+#include <netdev.h>
+#include <usb.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int dram_init(void)
+{
+	gd->ram_size = PHYS_SDRAM_SIZE;
+
+	return 0;
+}
+
+#define UART_PAD_CTRL  (PAD_CTL_PKE | PAD_CTL_PUE |		\
+	PAD_CTL_PUS_100K_UP | PAD_CTL_SPEED_MED |		\
+	PAD_CTL_DSE_40ohm   | PAD_CTL_SRE_FAST  | PAD_CTL_HYS)
+
+static const iomux_v3_cfg_t uart1_pads[] = {
+	MX6_PAD_GPIO1_IO04__UART1_TX	| MUX_PAD_CTRL(UART_PAD_CTRL),
+	MX6_PAD_ENET2_COL__UART1_RX	| MUX_PAD_CTRL(UART_PAD_CTRL),
+};
+
+static void setup_iomux_uart(void)
+{
+	imx_iomux_v3_setup_multiple_pads(uart1_pads, ARRAY_SIZE(uart1_pads));
+}
+
+#ifdef CONFIG_FSL_ESDHC
+#define USDHC_PAD_CTRL (PAD_CTL_PKE | PAD_CTL_PUE |		\
+	PAD_CTL_PUS_22K_UP  | PAD_CTL_SPEED_LOW |		\
+	PAD_CTL_DSE_80ohm   | PAD_CTL_SRE_FAST  | PAD_CTL_HYS)
+
+#define USDHC_POWER	IMX_GPIO_NR(1, 10)
+
+static const iomux_v3_cfg_t usdhc1_pads[] = {
+	MX6_PAD_SD1_CLK__USDHC1_CLK	| MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	MX6_PAD_SD1_CMD__USDHC1_CMD	| MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	MX6_PAD_SD1_DATA0__USDHC1_DATA0	| MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	MX6_PAD_SD1_DATA1__USDHC1_DATA1	| MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	MX6_PAD_SD1_DATA2__USDHC1_DATA2	| MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	MX6_PAD_SD1_DATA3__USDHC1_DATA3	| MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	MX6_PAD_GPIO1_IO10__GPIO1_IO_10	| MUX_PAD_CTRL(NO_PAD_CTRL),
+};
+
+static struct fsl_esdhc_cfg usdhc_cfg[1] = {
+	{ USDHC1_BASE_ADDR, 0, 4 },
+};
+
+int board_mmc_getcd(struct mmc *mmc)
+{
+	return 1;	/* Assume boot SD always present */
+}
+
+int board_mmc_init(bd_t *bis)
+{
+	imx_iomux_v3_setup_multiple_pads(usdhc1_pads, ARRAY_SIZE(usdhc1_pads));
+
+	gpio_request(USDHC_POWER, "usdhc_power");
+	gpio_direction_output(USDHC_POWER, 1);
+
+	usdhc_cfg[0].sdhc_clk = mxc_get_clock(MXC_ESDHC_CLK);
+
+	return fsl_esdhc_initialize(bis, &usdhc_cfg[0]);
+}
+#endif /* #ifdef CONFIG_FSL_ESDHC */
+
+#ifdef CONFIG_NAND_MXS
+#define GPMI_PAD_CTRL0 (PAD_CTL_PKE | PAD_CTL_PUE | PAD_CTL_PUS_100K_UP)
+#define GPMI_PAD_CTRL1 (PAD_CTL_DSE_40ohm | PAD_CTL_SPEED_MED | \
+			PAD_CTL_SRE_FAST)
+#define GPMI_PAD_CTRL2 (GPMI_PAD_CTRL0 | GPMI_PAD_CTRL1)
+
+iomux_v3_cfg_t gpmi_pads[] = {
+	MX6_PAD_NAND_CLE__RAWNAND_CLE		| MUX_PAD_CTRL(GPMI_PAD_CTRL2),
+	MX6_PAD_NAND_ALE__RAWNAND_ALE		| MUX_PAD_CTRL(GPMI_PAD_CTRL2),
+	MX6_PAD_NAND_WP_B__RAWNAND_WP_B		| MUX_PAD_CTRL(GPMI_PAD_CTRL2),
+	MX6_PAD_NAND_READY_B__RAWNAND_READY_B	| MUX_PAD_CTRL(GPMI_PAD_CTRL0),
+	MX6_PAD_NAND_CE0_B__RAWNAND_CE0_B	| MUX_PAD_CTRL(GPMI_PAD_CTRL2),
+	MX6_PAD_NAND_RE_B__RAWNAND_RE_B		| MUX_PAD_CTRL(GPMI_PAD_CTRL2),
+	MX6_PAD_NAND_WE_B__RAWNAND_WE_B		| MUX_PAD_CTRL(GPMI_PAD_CTRL2),
+	MX6_PAD_NAND_DATA00__RAWNAND_DATA00	| MUX_PAD_CTRL(GPMI_PAD_CTRL2),
+	MX6_PAD_NAND_DATA01__RAWNAND_DATA01	| MUX_PAD_CTRL(GPMI_PAD_CTRL2),
+	MX6_PAD_NAND_DATA02__RAWNAND_DATA02	| MUX_PAD_CTRL(GPMI_PAD_CTRL2),
+	MX6_PAD_NAND_DATA03__RAWNAND_DATA03	| MUX_PAD_CTRL(GPMI_PAD_CTRL2),
+	MX6_PAD_NAND_DATA04__RAWNAND_DATA04	| MUX_PAD_CTRL(GPMI_PAD_CTRL2),
+	MX6_PAD_NAND_DATA05__RAWNAND_DATA05	| MUX_PAD_CTRL(GPMI_PAD_CTRL2),
+	MX6_PAD_NAND_DATA06__RAWNAND_DATA06	| MUX_PAD_CTRL(GPMI_PAD_CTRL2),
+	MX6_PAD_NAND_DATA07__RAWNAND_DATA07	| MUX_PAD_CTRL(GPMI_PAD_CTRL2),
+};
+
+static void setup_gpmi_nand(void)
+{
+	struct mxc_ccm_reg *mxc_ccm = (struct mxc_ccm_reg *)CCM_BASE_ADDR;
+
+	/* config gpmi nand iomux */
+	imx_iomux_v3_setup_multiple_pads(gpmi_pads, ARRAY_SIZE(gpmi_pads));
+
+	setup_gpmi_io_clk(MXC_CCM_CS2CDR_QSPI2_CLK_PODF(0) |
+			  MXC_CCM_CS2CDR_QSPI2_CLK_PRED(3) |
+			  MXC_CCM_CS2CDR_QSPI2_CLK_SEL(3));
+
+	/* enable apbh clock gating */
+	setbits_le32(&mxc_ccm->CCGR0, MXC_CCM_CCGR0_APBHDMA_MASK);
+}
+#endif /* CONFIG_NAND_MXS */
+
+#ifdef CONFIG_FEC_MXC
+#define ENET_PAD_CTRL		(PAD_CTL_PUS_100K_UP | PAD_CTL_PUE |	\
+				 PAD_CTL_SPEED_HIGH  |			\
+				 PAD_CTL_DSE_48ohm   | PAD_CTL_SRE_FAST)
+#define ENET_RX_PAD_CTRL	(PAD_CTL_PKE | PAD_CTL_PUE |		\
+				 PAD_CTL_SPEED_HIGH | PAD_CTL_SRE_FAST)
+
+#define ETH_PHY_POWER	IMX_GPIO_NR(1, 13)
+#define ETH_PHY_RESET	IMX_GPIO_NR(2, 17)
+
+static iomux_v3_cfg_t const fec1_pads[] = {
+	MX6_PAD_GPIO1_IO05__ENET1_REF_CLK1	| MUX_PAD_CTRL(ENET_PAD_CTRL) |
+						  MUX_MODE_SION,
+	MX6_PAD_ENET1_MDC__ENET1_MDC		| MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_ENET1_MDIO__ENET1_MDIO		| MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_RGMII1_RX_CTL__ENET1_RX_EN   | MUX_PAD_CTRL(ENET_RX_PAD_CTRL),
+	MX6_PAD_RGMII1_RD0__ENET1_RX_DATA_0  | MUX_PAD_CTRL(ENET_RX_PAD_CTRL),
+	MX6_PAD_RGMII1_RD1__ENET1_RX_DATA_1  | MUX_PAD_CTRL(ENET_RX_PAD_CTRL),
+	MX6_PAD_RGMII1_TX_CTL__ENET1_TX_EN	| MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_RGMII1_TD0__ENET1_TX_DATA_0	| MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_RGMII1_TD1__ENET1_TX_DATA_1	| MUX_PAD_CTRL(ENET_PAD_CTRL),
+
+	/* FEC_PWR_EN - ENET PHY Power Enable */
+	MX6_PAD_GPIO1_IO13__GPIO1_IO_13		| MUX_PAD_CTRL(NO_PAD_CTRL),
+
+	/* ENET_RST - ENET Reset GPIO */
+	MX6_PAD_KEY_ROW2__GPIO2_IO_17		| MUX_PAD_CTRL(NO_PAD_CTRL),
+};
+
+static void setup_iomux_fec1(void)
+{
+	imx_iomux_v3_setup_multiple_pads(fec1_pads, ARRAY_SIZE(fec1_pads));
+}
+
+static int setup_fec(void)
+{
+	u32 *gpr = (u32 *)IOMUXC_GPR_BASE_ADDR;
+	int err;
+
+	/* clear gpr1[13], set gpr1[17] to select anatop clock output */
+	clrbits_le32(&gpr[1], 1 << 13);
+	setbits_le32(&gpr[1], 1 << 17);
+
+	err = enable_fec_anatop_clock(0, ENET_50MHZ);
+	if (err)
+		return err;
+
+	gpio_request(ETH_PHY_POWER, "eth_phy_power");
+	gpio_request(ETH_PHY_RESET, "eth_phy_reset");
+
+	/* Hold PHY in reset */
+	gpio_direction_output(ETH_PHY_RESET, 0);
+
+	/* Required to avoid system reset on cold boot */
+	mdelay(10);
+
+	/* Power up PHY */
+	gpio_direction_output(ETH_PHY_POWER, 0);
+	mdelay(30);
+
+	/* Out of reset */
+	gpio_set_value(ETH_PHY_RESET, 1);
+	mdelay(1);
+
+	return 0;
+}
+
+int board_eth_init(bd_t *bis)
+{
+	setup_iomux_fec1();
+	setup_fec();
+
+	return fecmxc_initialize_multi(bis, 0, CONFIG_FEC_MXC_PHYADDR,
+				       IMX_FEC_BASE);
+}
+
+#endif /* CONFIG_FEC_MXC */
+
+#ifdef CONFIG_USB_EHCI_MX6
+#define USBNC_OTG1_CTRL_REG_OFFSET	0x800
+#define UCTRL_PWR_POL			(1 << 9)
+
+#define USB_OTG1_POWER	IMX_GPIO_NR(1, 9)
+#define USB_OTG2_POWER	IMX_GPIO_NR(1, 12)
+
+static const iomux_v3_cfg_t usb_otg_pads[] = {
+	/* OTG1 */
+	MX6_PAD_GPIO1_IO09__GPIO1_IO_9		| MUX_PAD_CTRL(NO_PAD_CTRL),
+
+	/* OTG2 */
+	MX6_PAD_GPIO1_IO12__GPIO1_IO_12		| MUX_PAD_CTRL(NO_PAD_CTRL)
+};
+
+static void setup_usb(void)
+{
+	imx_iomux_v3_setup_multiple_pads(usb_otg_pads,
+					 ARRAY_SIZE(usb_otg_pads));
+}
+
+int board_usb_phy_mode(int port)
+{
+	/* All ports are host on CANvu500 */
+	return USB_INIT_HOST;
+}
+
+int board_ehci_hcd_init(int port)
+{
+	u32 *usbnc_usb_ctrl;
+
+	if (port > 1)
+		return -EINVAL;
+
+	/* Set Power polarity */
+	usbnc_usb_ctrl =
+		(u32 *)(USB_BASE_ADDR + USBNC_OTG1_CTRL_REG_OFFSET) + port;
+	setbits_le32(usbnc_usb_ctrl, UCTRL_PWR_POL);
+
+	if (port == 0) {
+		gpio_request(USB_OTG1_POWER, "usb_otg1_power");
+		gpio_direction_output(USB_OTG1_POWER, 1);
+	} else {
+		gpio_request(USB_OTG2_POWER, "usb_otg2_power");
+		gpio_direction_output(USB_OTG2_POWER, 1);
+	}
+
+	return 0;
+}
+#endif /* CONFIG_USB_EHCI_MX6 */
+
+int board_early_init_f(void)
+{
+	setup_iomux_uart();
+
+	return 0;
+}
+
+int board_init(void)
+{
+	/* address of boot parameters */
+	gd->bd->bi_boot_params = PHYS_SDRAM + 0x100;
+
+#ifdef CONFIG_NAND_MXS
+	setup_gpmi_nand();
+#endif
+
+#ifdef CONFIG_USB_EHCI_MX6
+	setup_usb();
+#endif
+
+	return 0;
+}
+
+#define ATTN_BTN IMX_GPIO_NR(2, 19)
+
+int board_late_init(void)
+{
+	struct src *src = (struct src *)SRC_BASE_ADDR;
+	int bmode = (src->sbmr2 & SRC_SBMR2_BMOD_MASK) >> SRC_SBMR2_BMOD_SHIFT;
+
+	gpio_request(ATTN_BTN, "attn_btn");
+	gpio_direction_input(ATTN_BTN);
+
+	if (bmode == SRC_SBMR2_BMOD_SERIAL || !gpio_get_value(ATTN_BTN)) {
+		set_default_env(NULL);
+		env_set("recovery", "1");
+	} else {
+		env_set("recovery", "0");
+	}
+
+	return 0;
+}
+
+int checkboard(void)
+{
+	puts("Board: CANVU500\n");
+
+	return 0;
+}
---
+++ include/configs/canvu500.h
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2016, CANtronik Ltd.
+ *
+ * Configuration settings for the CANtronik CANvu500 board.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include "mx6_common.h"
+#include "imx6_spl.h"
+
+#define CONFIG_SYS_HZ			1000
+#define CONFIG_SYS_MALLOC_LEN		SZ_32M
+
+#define CONFIG_MXC_UART_BASE		UART1_BASE
+
+/* MMC Configs */
+#define CONFIG_SYS_FSL_ESDHC_ADDR	0
+#define CONFIG_SYS_FSL_USDHC_NUM	1
+
+/* Network */
+#define CONFIG_FEC_MXC
+#define CONFIG_MII
+#define IMX_FEC_BASE			ENET_BASE_ADDR
+#define CONFIG_FEC_XCV_TYPE             RMII
+#define CONFIG_ETHPRIME                 "FEC0"
+#define CONFIG_FEC_MXC_PHYADDR          0
+
+/* Physical Memory Map */
+#define CONFIG_NR_DRAM_BANKS		1
+#define PHYS_SDRAM			MMDC0_ARB_BASE_ADDR
+#define PHYS_SDRAM_SIZE			SZ_256M
+#define CONFIG_SYS_SDRAM_BASE		PHYS_SDRAM
+#define CONFIG_SYS_INIT_RAM_ADDR	IRAM_BASE_ADDR
+#define CONFIG_SYS_INIT_RAM_SIZE	IRAM_SIZE
+
+#define CONFIG_SYS_INIT_SP_OFFSET 	(CONFIG_SYS_INIT_RAM_SIZE - GENERATED_GBL_DATA_SIZE)
+#define CONFIG_SYS_INIT_SP_ADDR 	(CONFIG_SYS_INIT_RAM_ADDR + CONFIG_SYS_INIT_SP_OFFSET)
+
+/* NAND stuff */
+#define CONFIG_SYS_MAX_NAND_DEVICE	1
+#define CONFIG_SYS_NAND_BASE		0x40000000
+#define CONFIG_SYS_NAND_5_ADDR_CYCLE
+#define CONFIG_SYS_NAND_ONFI_DETECTION
+
+/* DMA stuff, needed for GPMI/MXS NAND support */
+#define CONFIG_APBH_DMA
+#define CONFIG_APBH_DMA_BURST
+#define CONFIG_APBH_DMA_BURST8
+
+#define CONFIG_ENV_OFFSET		SZ_4M
+#define CONFIG_ENV_SECT_SIZE		SZ_256K
+#define CONFIG_ENV_SIZE			CONFIG_ENV_SECT_SIZE
+
+#define CONFIG_MTD_DEVICE
+#define CONFIG_MTD_PARTITIONS
+
+/* USB Configs */
+#define CONFIG_EHCI_HCD_INIT_AFTER_RESET
+#define CONFIG_MXC_USB_PORTSC		(PORT_PTS_UTMI | PORT_PTS_PTW)
+#define CONFIG_MXC_USB_FLAGS		0
+#define CONFIG_USB_MAX_CONTROLLER_COUNT	2
+
+#define CONFIG_BOOTCOMMAND "run recovercmd; run setroot setargs ubiboot"
+
+#define CONFIG_ENV_VARS_UBOOT_CONFIG
+#define CONFIG_EXTRA_ENV_SETTINGS					\
+	"bootargs=\0"							\
+	"console=ttymxc0\0"						\
+	"mtdids=" CONFIG_MTDIDS_DEFAULT "\0"				\
+	"mtdparts=" CONFIG_MTDPARTS_DEFAULT "\0"			\
+	"netboot=dhcp ${board}/fitImage && bootm\0"			\
+	"recovercmd=test ${recovery} = 1 && run usbboot\0"		\
+	"rdaddr=0x88000000\0"						\
+	"setargs=setenv bootargs ${bootargs} "				\
+		"console=${console},${baudrate} "			\
+		"${mtdparts} ubi.mtd=ubi "				\
+		"root=${root} rootfstype=ubifs ${runlevel}\0"		\
+	"setroot=setenv root ubi0:rootfs${swversion}\0"			\
+	"swversion=1\0"							\
+	"ubiboot=ubi part ubi && ubifsmount ${root} && ubifsload ${loadaddr} boot/fitImage && bootm\0" \
+	"usbboot=usb reset && load usb 0 ${loadaddr} boot.scr && source ${loadaddr}; sleep 2; run usbboot\0"
+
+#endif				/* __CONFIG_H */
