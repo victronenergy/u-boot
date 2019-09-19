13a4b7d8a69e76fe5a2c352500e2fef82ec9bb2b
Mans Rullgard <mans@mansr.com>
2019-09-19 18:46:33 +0100

    nanopi-victron: add Easysolar A9 board id

--- include/configs/nanopi_victron.h
+++ include/configs/nanopi_victron.h
@@ -8,6 +8,7 @@
 #define VICTRON_ID_TABLE \
 	"board_53454556=setenv fdtfile sun8i-h3-nanopi-easysolar.dtb\0" \
 	"board_504d4556=setenv fdtfile sun8i-h3-nanopi-easysolar.dtb; setenv model MultiPlus-II\0" \
+	"board_39534556=setenv fdtfile sun8i-h3-nanopi-easysolar-a9.dtb\0" \
 	"board_584d4556=setenv fdtfile sun8i-h3-nanopi-maxi-gx.dtb\0" \
 	"board_47504556=setenv fdtfile sun8i-h3-nanopi-paygo.dtb\0" \
 	"board_3456584d=setenv fdtfile sun8i-h3-nanopi-v4-smart-hub.dtb\0"
