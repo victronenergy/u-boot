f587e351f3c734f4c457cf62e9ccc3f6b772cf8e
Mans Rullgard <mans@mansr.com>
2020-05-13 15:58:01 +0100

    nanopi-victron: add maxi/multiplus a10 board ids

--- include/configs/nanopi_victron.h
+++ include/configs/nanopi_victron.h
@@ -11,8 +11,10 @@
 	"board_39534556=setenv fdtfile sun8i-h3-nanopi-easysolar-a9.dtb\0" \
 	"board_39504d56=setenv fdtfile sun8i-h3-nanopi-easysolar-a9.dtb; setenv model MultiPlus-II\0" \
 	"board_41534556=setenv fdtfile sun8i-h3-nanopi-easysolar-a10.dtb\0" \
+	"board_41504d56=setenv fdtfile sun8i-h3-nanopi-easysolar-a10.dtb; setenv model MultiPlus-II\0" \
 	"board_584d4556=setenv fdtfile sun8i-h3-nanopi-maxi-gx.dtb\0" \
 	"board_39584d56=setenv fdtfile sun8i-h3-nanopi-maxi-gx-a9.dtb\0" \
+	"board_41584d56=setenv fdtfile sun8i-h3-nanopi-maxi-gx-a10.dtb\0" \
 	"board_47504556=setenv fdtfile sun8i-h3-nanopi-paygo.dtb\0" \
 	"board_3456584d=setenv fdtfile sun8i-h3-nanopi-v4-smart-hub.dtb\0"
 
