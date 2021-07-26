2d98f4f1573df1f2b032cdb3148b495488d5aa49
Mans Rullgard <mans@mansr.com>
2021-07-26 12:34:14 +0100

    einstein-victron: add cerbo b1 board id

--- include/configs/einstein_victron.h
+++ include/configs/einstein_victron.h
@@ -7,6 +7,7 @@
 #define VICTRON_ID_EEPROM_BUS 1
 #define VICTRON_ID_TABLE \
 	"board_42434556=setenv fdtfile sun7i-a20-einstein-ccgx2.dtb\0" \
-	"board_53434556=setenv fdtfile sun7i-a20-einstein-cerbo-gx-s.dtb\0"
+	"board_53434556=setenv fdtfile sun7i-a20-einstein-cerbo-gx-s.dtb\0" \
+	"board_31424356=setenv fdtfile sun7i-a20-einstein-cerbo-b1.dtb\0"
 
 #endif
