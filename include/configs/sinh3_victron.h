658fff734b080506d1de451d7280ec0705305563
Mans Rullgard <mans@mansr.com>
2021-12-22 15:02:39 +0000

    configs: add victron ekrano board

---
+++ include/configs/sinh3_victron.h
@@ -0,0 +1,11 @@
+#ifndef _SINH3_VICTRON_H
+#define _SINH3_VICTRON_H
+
+#include "sun8i.h"
+#include "sunxi_victron_common.h"
+
+#define VICTRON_ID_EEPROM_BUS 0
+#define VICTRON_ID_TABLE \
+	"board_524b4556=setenv fdtfile sun8i-h3-sinh3-ekrano.dtb\0"
+
+#endif
