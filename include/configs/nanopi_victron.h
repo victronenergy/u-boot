9cc792d7f453777b4a3667035a38f9a16f19c027
Mans Rullgard <mans@mansr.com>
2024-07-12 16:57:11 +0100

    nanopi-victron: add tigro board id

--- include/configs/nanopi_victron.h
+++ include/configs/nanopi_victron.h
@@ -19,6 +19,7 @@
 	"board_3456584d=setenv fdtfile sun8i-h3-nanopi-v4-smart-hub.dtb\0" \
 	"board_524b4556=setenv fdtfile sun8i-h3-sinh3-ekrano.dtb\0" \
 	"board_41524b56=setenv fdtfile sun8i-h3-sinh3-ekrano-a10.dtb\0" \
-	"board_43554e56=setenv fdtfile sun8i-h3-sinh3-nucleo.dtb\0"
+	"board_43554e56=setenv fdtfile sun8i-h3-sinh3-nucleo.dtb\0" \
+	"board_52475456=setenv fdtfile sun8i-h3-sinh3-tigro.dtb\0"
 
 #endif
